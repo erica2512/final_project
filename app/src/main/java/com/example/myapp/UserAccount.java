e7e36c734ffe0f5675009a22524535d9bf864a95
erica2512 <012banada@naver.com>
2022-04-21 02:12:04 +0900

    login

---
+++ app/src/main/java/com/example/myapp/UserAccount.java
@@ -0,0 +1,36 @@
+package com.example.myapp;
+
+/**
+ * 사용자 계정 정보
+ */
+public class UserAccount {
+    private String idToken;     //firebase Uid(고유 토큰 정보)
+    private String emailId;     //이메일 아니디
+    private String password;    //비밀번호
+
+    public UserAccount() { }
+
+    public String getIdToken() {
+        return idToken;
+    }
+
+    public void setIdToken(String idToken) {
+        this.idToken = idToken;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
