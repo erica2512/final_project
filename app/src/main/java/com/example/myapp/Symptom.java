e4f86f8e75d98ae949a3fe0c2b3d4c54fdb47d17
dursm9995 <egim9995@gmail.com>
2022-05-13 16:30:20 +0900

    20220513 commit

--- app/src/main/java/com/example/myapp/Symptom.java
+++ app/src/main/java/com/example/myapp/Symptom.java
@@ -9,6 +9,6 @@ public class Symptom extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_symptom);
+        setContentView(R.layout.activity_record_symptom);
     }
 }
